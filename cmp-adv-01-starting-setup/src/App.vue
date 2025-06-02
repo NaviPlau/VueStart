9253080325892fb0ec2e9803e439983a42ed4bdb
NaviPlau <naviplau@outlook.com>
2025-06-02 22:47:54 +0200

    chore: learn about slots in vue

--- cmp-adv-01-starting-setup/src/App.vue
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -1,25 +1,37 @@
 <template>
-  <div>
-    <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
-  </div>
+    <the-header/>
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals> -->
+    <component :is="selectedComponent"></component>
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
+import ActiveGoals from './components/ActiveGoals.vue';
+import ManageGoals from './components/ManageGoals.vue';
 export default {
+  components: {
+    TheHeader,
+    ActiveGoals,
+    ManageGoals
+  },
+  
   data() {
     return {
+      selectedComponent: 'active-goals',
       activeUser: {
         name: 'Maximilian Schwarzmüller',
         description: 'Site owner and admin',
         role: 'admin',
       },
     };
+  },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+    },
   },
 };
 </script>
---
+++ cmp-adv-01-starting-setup/src/components/CourseGoals.vue
@@ -0,0 +1,14 @@
+<template>
+  <ul v-for="goal in goals" :key="goal">
+    <slot :item="goal" another-prop="..."></slot></ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      goals: ['finish the course', 'learn vue'],
+    };
+  }
+}
+</script>
